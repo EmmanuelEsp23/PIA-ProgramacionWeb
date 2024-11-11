2879652acceb6be11a55383106126c7601a3294d
Deyanira Simon Acota <deyanira.simonacs@uanl.edu.mx>
2024-11-10 21:36:22 -0600

    Se añadió un view, controller y layout(navbar dentro del layout) para el Dashboard

---
+++ PIA-PWEB/PIA-PWEB/Controllers/DashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PIA_PWEB.Models;
+using System.Diagnostics;
+
+
+namespace PIA_PWEB.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View("dashboard");
+        }
+    }
+}
